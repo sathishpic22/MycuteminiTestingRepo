119b68ded75f0e1705216a59d64493c73d792c93
sathishpic22 <58845085+sathishpic22@users.noreply.github.com>
2021-09-15 16:08:18 +0530

    UserTable Commit

--- src/main/java/com/pipe/model/UserTable.java
+++ src/main/java/com/pipe/model/UserTable.java
@@ -1,49 +1,56 @@
 package com.pipe.model;
 
+import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
-
-import org.springframework.stereotype.Component;
+import javax.persistence.Table;
 
 @Entity
-@Component
+@Table(name = "usertable")
 public class UserTable {
+	
 	@Id
-	private String username;
+	@Column(name = "username")
+    private String username;
+
+	@Column(name = "password")
 	private String password;
+
+	@Column(name = "usertype")
 	private String userType;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
 	public UserTable() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public UserTable(String username, String password, String userType) {
-		super();
-		this.username = username;
-		this.password = password;
-		this.userType = userType;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getUserType() {
-		return userType;
-	}
-	public void setUserType(String userType) {
-		this.userType = userType;
-	}
 	
 	
+
 	
-	
-	
+
 }
